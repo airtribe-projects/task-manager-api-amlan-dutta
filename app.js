6967e3c99be28fa05799a3837b55e399596efa47
amlan-dutta <amlan-dutta>
2025-12-28 14:15:45 +0530

    PR #1

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require('express');
+const fs = require("fs");
+const path = require("path");
 const app = express();
 const port = 3000;
+const taskRoutes = require("./taskRoutes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 app.listen(port, (err) => {
     if (err) {
         return console.log('Something bad happened', err);
     }
+    app.use("/tasks", taskRoutes);
     console.log(`Server is listening on ${port}`);
+    //console.log(data);
 });
-
 
 
 module.exports = app;
